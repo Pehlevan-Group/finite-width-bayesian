9aa1f72d040c6a6c8ff9dc2315629c653be627ee
canatara <60362246+canatara@users.noreply.github.com>
2021-05-26 22:19:30 -0400

    Update langevin.py

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -108,7 +108,7 @@ class Langevin():
               w.grad = None
 
         # Compute averaged weight matrices online once loss = noise
-        if loss.item() < self.n0*self.nd/(2*beta) * 100:
+        if loss.item() < self.n0*self.nd/(2*beta) * 1000:
           self.avg_count += 1
           for i in range(len(h)):
             self.W_avg[i] += self.W[i].detach()
