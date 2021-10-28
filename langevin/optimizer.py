92829e0c94ba40312fcfb0bd13ab43f1b84a8840
canatara <60362246+canatara@users.noreply.github.com>
2021-10-28 18:53:53 -0400

    Update optimizer.py

--- langevin/optimizer.py
+++ langevin/optimizer.py
@@ -7,16 +7,17 @@ from jax.experimental.optimizers import optimizer, make_schedule, Optimizer
 def sgld(step_size, beta, batch_factor):
     step_size = make_schedule(step_size)
     def init(x0):
-        return x0
+        key = random.PRNGKey(1)
+        return x0, key
     
     def update(i, g, state):
-        x = state
-        key = random.PRNGKey(i)
+        x, key = state
+        _,key = random.split(key)
         x -= step_size(i) * (batch_factor*g + x/beta) + jnp.sqrt(2*step_size(i)/beta) * random.normal(key, shape=g.shape)
         return x
     
     def get_params(state):
-        x = state
+        x,_ = state
         return x
     
     return Optimizer(init, update, get_params)
